52b6c0ea1abf0e6f749455cdf6681cfbc1c9ec81
Aleksandras Urbonas <aleksandras.urbonas@gmail.com>
2021-04-19 18:53:06 +0300

    word count: working
    Signed-off-by: Aleksandras Urbonas <aleksandras.urbonas@gmail.com>

--- word_count/word_count__mongodb__count_2.js
+++ word_count/word_count__mongodb__count_2.js
@@ -1,10 +1,13 @@
 // title: word count - mongodb - count # 2
-// v: 2021-04-18T2215 AU
+// v: 2021-04-19T1848 AU
+
 db.words.aggregate([
-	{ $project : { "words_list" : { $split: ["$text", " "] } } }
-,	{ $unwind : $words_list }
-,	{ $match : { "words_list" : /[A-Za-z]/ } }
-,	{ $group : { "_id": { "Text_id": $_id }, "count": { $sum: 1 } } }
-,	{ $sort : { "count" : -1 } }
+	{ $project : { words_list : { $split: ["$text", " "] } } }
+,	{ $unwind : "$words_list" }
+,	{ $match : { words_list : /[A-Za-z]/ } }
+,	{ $group : { _id: { "Text_id": "$_id" }, count: { "$sum": 1 } } }
+//,	{ $sort : { count : -1 } } // v1: one variable
+,	{ $sort : { count : 1, _id : -1 } } // v2: multiple variables
+//,	{ $limit: 2 }
 ])
 ;
